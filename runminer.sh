d34533ddc963e4bc4f8bd0d5e7fcd9b3690a7564
Andre Fernandes <andre@vertigo.com.br>
2016-09-13 10:43:39 -0300

    miner

--- runminer.sh
+++ runminer.sh
@@ -2,4 +2,4 @@
 NODE_NAME=$1
 NODE_NAME=${NODE_NAME:-"miner1"}
 ETHERBASE=${ETHERBASE:-"0x0000000000000000000000000000000000000001"}
-./runnode.sh $NODE_NAME --mine --minerthreads=1 --etherbase=$ETHERBASE
+./runnode.sh $NODE_NAME --mine --minerthreads=1 --etherbase="$ETHERBASE"
